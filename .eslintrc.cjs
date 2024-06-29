752d5843a29f2c2bfaaa33b10c12777af5efd91f
bisari31 <bisari31@gmail.com>
2024-06-30 01:03:31 +0900

    chore: jsx-a11y 접근성rules 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,6 +7,7 @@ module.exports = {
     'plugin:react-hooks/recommended',
     'plugin:react/recommended',
     'plugin:@tanstack/eslint-plugin-query/recommended',
+    'plugin:jsx-a11y/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
