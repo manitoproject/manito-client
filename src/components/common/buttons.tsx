ddadf7b50399c09333e4db3a894771d9aa66dab3
bisari31 <bisari31@gmail.com>
2024-07-03 21:49:04 +0900

    design: kakao,default button 크기 수정

--- src/components/common/buttons.tsx
+++ src/components/common/buttons.tsx
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled';
 
-import KakaoLogo from '../../assets/svg/icons/kakao-logo.svg';
+import { KakaoLogo } from '../../assets/svg';
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   mb?: number;
@@ -26,21 +26,19 @@ const StyledButton = styled.button<{ mb?: number }>`
     color: ${(props) => props.theme.colors.gray[500]};
   }
   font-weight: bold;
-  padding: 14px 16px;
   border-radius: 4px;
+  height: 48px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   width: 100%;
   color: ${(props) => props.theme.colors.white};
   background-color: ${(props) => props.theme.colors.black};
 `;
 
 const StyledKakaoButton = styled(StyledButton)`
-  display: flex;
-  align-items: center;
   gap: 8px;
   font-size: 15px;
-  font-weight: 600;
-  line-height: 150%;
-  justify-content: center;
   font-weight: 600;
   font-family: ${(props) => props.theme.fontFamily.AppleSDGothicNeo};
   background-color: ${(props) => props.theme.colors.kakao};
