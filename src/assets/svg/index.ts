5eca51f0562ba5778d02f6792dabd78ee9b5b005
bisari31 <bisari31@gmail.com>
2024-07-04 23:50:38 +0900

    rename: svg link에서 clip 으로 변경

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -1,8 +1,8 @@
 export { default as AddCircle } from './add-circle.svg';
+export { default as Clip } from './clip.svg';
 export { default as Close } from './close.svg';
 export { default as HamburgerMenu } from './hamburger-menu.svg';
 export { default as KakaoLogo } from './kakao-logo.svg';
 export { default as LeftChevron } from './left-chevron.svg';
-export { default as Link } from './link.svg';
 export { default as RadioButton } from './radio-button.svg';
 export { default as RadioButtonActive } from './radio-button-active.svg';
