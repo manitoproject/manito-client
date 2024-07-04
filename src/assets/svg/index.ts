5f37e7ded62a61834b9c12f6ea8df7b9c3425fb8
bisari31 <bisari31@gmail.com>
2024-07-05 05:32:25 +0900

    design: svg 파일 저장

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -1,8 +1,12 @@
 export { default as AddCircle } from './add-circle.svg';
 export { default as Clip } from './clip.svg';
 export { default as Close } from './close.svg';
+export { default as Document } from './document.svg';
 export { default as HamburgerMenu } from './hamburger-menu.svg';
+export { default as Home } from './home.svg';
 export { default as KakaoLogo } from './kakao-logo.svg';
 export { default as LeftChevron } from './left-chevron.svg';
+export { default as Logout } from './logout.svg';
+export { default as Profile } from './profile.svg';
 export { default as RadioButton } from './radio-button.svg';
 export { default as RadioButtonActive } from './radio-button-active.svg';
