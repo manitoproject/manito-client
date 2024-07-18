bc21b4a16fa57af5475ac15910dedf9d72db47d3
bisari31 <bisari31@gmail.com>
2024-07-18 22:11:38 +0900

    feat: proxy 설정

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,30 @@ import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    svgr({
-      // svgr options: https://react-svgr.com/docs/options/
-      svgrOptions: {
-        exportType: 'default',
-        ref: true,
-        svgo: false,
-        titleProp: true,
+export default () => {
+  return defineConfig({
+    plugins: [
+      react(),
+      svgr({
+        // svgr options: https://react-svgr.com/docs/options/
+        svgrOptions: {
+          exportType: 'default',
+          ref: true,
+          svgo: false,
+          titleProp: true,
+        },
+        include: '**/*.svg',
+      }),
+    ],
+    server: {
+      proxy: {
+        '/api': {
+          target: 'https://api.manito.service.kimjunyoung.com',
+          // secure: false,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+          changeOrigin: true,
+        },
       },
-      include: '**/*.svg',
-    }),
-  ],
-});
+    },
+  });
+};
