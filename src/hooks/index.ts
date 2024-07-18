129681bf33a0d4885d08a92ee9db519af95dbae8
bisari31 <bisari31@gmail.com>
2024-07-18 15:26:27 +0900

    feat: redux 셋팅

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,10 @@
+import { useDispatch, useSelector } from 'react-redux';
+
+import { AppDispatch, RootState } from '../store';
+
 export { default as useDisableScroll } from './useDisableScroll';
 export { default as useNameForm } from './useNameForm';
 export { default as useOutsideClick } from './useOutsideClick';
+
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
+export const useAppSelector = useSelector.withTypes<RootState>();
--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
 
 import { enableMocking } from './mocks/browser';
 import router from './router';
+import store from './store';
 import global from './styles/global';
 import theme from './styles/theme';
 
@@ -18,7 +20,9 @@ enableMocking().then(() => {
       <ThemeProvider theme={theme}>
         <Global styles={global} />
         <QueryClientProvider client={queryClient}>
-          <RouterProvider router={router()} />
+          <Provider store={store}>
+            <RouterProvider router={router()} />
+          </Provider>
           <ReactQueryDevtools />
         </QueryClientProvider>
       </ThemeProvider>
