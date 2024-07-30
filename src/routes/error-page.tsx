ed1b8bd8b7ac8fa7a7d1c52141c3d382f28364ed
bisari31 <bisari31@gmail.com>
2024-07-30 23:45:30 +0900

    feat: 404 페이지 추가

--- src/routes/error-page.tsx
+++ src/routes/error-page.tsx
@@ -1,7 +1,58 @@
-import { useRouteError } from 'react-router-dom';
+// import { useRouteError } from 'react-router-dom';
+
+import styled from '@emotion/styled';
+
+import { Manito } from '../assets/svg/icons';
 
 export default function ErrorPage() {
-  const error = useRouteError();
-  console.error(error);
-  return <div> 404 not found</div>;
+  // const error = useRouteError();
+  // console.error(error);
+
+  return (
+    <StyledErrorWrapper>
+      <div>
+        <h1>MANITO ERROR</h1>
+        <Manito />
+        <p>
+          페이지를 찾을 수 없습니다.
+          <br />
+          잘못된 주소를 입력하셨거나, 요청하신 페이지의 주소가
+          <br />
+          변경, 삭제되어 찾을 수 없습니다.
+        </p>
+      </div>
+    </StyledErrorWrapper>
+  );
 }
+
+const StyledErrorWrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  background-color: ${({ theme }) => theme.colors['gray-100']};
+  align-items: center;
+  justify-content: center;
+  div {
+    height: 100%;
+    justify-content: center;
+    background-color: ${({ theme }) => theme.colors['white']};
+    width: ${({ theme }) => theme.sizes.mobile};
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    gap: 24px;
+    h1 {
+      font-family: ${({ theme }) => theme.fontFamily.Cafe24Ssurround};
+      font-size: 32px;
+      font-weight: 700;
+      color: ${({ theme }) => theme.colors.black};
+    }
+
+    p {
+      line-height: 24px;
+      text-align: center;
+      font-size: 14px;
+      color: ${({ theme }) => theme.colors['gray-700']};
+    }
+  }
+`;
