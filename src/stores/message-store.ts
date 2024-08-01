e7cfcb1392ba125ab09bb930150e6e98ab8e1e47
bisari31 <bisari31@gmail.com>
2024-08-01 15:09:04 +0900

    feat: 메시지 8개 초과시 8개 재생성 로직 추가

--- src/stores/message-store.ts
+++ src/stores/message-store.ts
@@ -6,6 +6,7 @@ import { ThemeKey } from '../constants/theme-list';
 import { Message } from '../types/message';
 
 const INIT_LIST = Array(8).fill(null);
+const MAX_LIST_LENGTH = 56;
 
 interface MessageState {
   list: Array<null | Message | Pick<Message, 'theme'>>;
@@ -32,12 +33,31 @@ const useMessageStore = create<MessageState>()(
     setActiveEmojiIndex: (i: number | null) => set({ activeEmojiIndex: i }),
     setActiveMessageIndex: (i: number | null) => set({ activeMessageIndex: i }),
     snycList: (serverData?: Message[]) =>
-      set((state) => ({
-        list: state.list.map((prev, i) => {
-          const item = serverData?.find((item) => item.position === i);
-          return item ?? prev;
-        }),
-      })),
+      set((state) => {
+        if (!serverData?.length) return { list: state.list };
+        const currentLength = state.list.length;
+        const serverDataLength = serverData.length;
+        const additionalData = Array(
+          currentLength - Math.ceil(serverDataLength / 8),
+        ).fill(null);
+        let newList = [...state.list];
+        if (
+          serverDataLength >= currentLength &&
+          serverDataLength < MAX_LIST_LENGTH
+        ) {
+          if (serverDataLength % 8 !== 0) {
+            newList = [...serverData, ...additionalData];
+          } else {
+            newList = [...serverData, ...INIT_LIST];
+          }
+        }
+        return {
+          list: newList.map((prev, i) => {
+            const item = serverData?.find((item) => item.position === i);
+            return item ?? prev;
+          }),
+        };
+      }),
     addList: (theme: ThemeKey) =>
       set((state) => ({
         list: state.list.map((prev, i) => {
