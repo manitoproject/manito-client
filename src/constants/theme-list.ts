33823a95775d5dbaa8ce543d0a1cbc2a0373bde6
bisari31 <bisari31@gmail.com>
2024-08-07 00:27:28 +0900

    fix: rolling-setup theme list bg 수정

--- src/constants/theme-list.ts
+++ src/constants/theme-list.ts
@@ -1,4 +1,8 @@
+import animalBg from '../assets/imgs/bg/animal-theme@4x-100.jpg';
+import natureBg from '../assets/imgs/bg/nature-theme@4x-100.jpg';
+import spaceBg from '../assets/imgs/bg/space-theme@4x-100.jpg';
 import { Message } from '../types/message';
+
 const ROLLING_THEME_NAME: RollingThemeName[] = ['nature', 'space', 'animal'];
 
 export const getRollingThemeName = (
@@ -12,17 +16,17 @@ const themeList: Theme[] = [
   {
     themeKor: '우주',
     themeEng: 'space',
-    img: '/src/assets/imgs/bg/space-theme@4x-100.jpg',
+    img: spaceBg,
   },
   {
     themeKor: '자연',
     themeEng: 'nature',
-    img: '/src/assets/imgs/bg/nature-theme@4x-100.jpg',
+    img: natureBg,
   },
   {
     themeKor: '멍냥',
     themeEng: 'animal',
-    img: '/src/assets/imgs/bg/animal-theme@4x-100.jpg',
+    img: animalBg,
   },
 ];
 
