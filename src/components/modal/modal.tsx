c6e7d5ed73c8b6dc52c44f0f04a8a80e980952b4
bisari31 <bisari31@gmail.com>
2024-08-18 00:59:57 +0900

    style: 편지 작성 모달 설명 수정

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -51,7 +51,11 @@ function RadioForm(
     <StyledRadioFormWrapper>
       <StyledRadioFormTitleWrapper>
         <h1>작성한 편지를 공개 하시겠어요?</h1>
-        <p>익명 선택시 익명, 공개 선택시 설정한 이름으로 공개됩니다.</p>
+        <p>
+          익명 선택시 임의의 이름을 작성해서 공개 하실 수 있으며,
+          <br />
+          공개 선택시엔 이름으로 공개됩니다.
+        </p>
       </StyledRadioFormTitleWrapper>
       <div>
         {LABELS.map((item, i) => (
