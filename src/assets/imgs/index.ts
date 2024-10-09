f69ef6cd18ccd4e2d1f477c6662344ceae474c40
bisari31 <bisari31@gmail.com>
2024-10-10 01:46:00 +0900

    design: 이미지 업로드

--- src/assets/imgs/index.ts
+++ src/assets/imgs/index.ts
@@ -1,4 +1,5 @@
-export { default as MakeCakeBadge } from './badge/makecake.svg';
-export { default as RollingBadge } from './badge/rolling.svg';
+export { default as MakeCakeBadge } from './badge/makecake-content-png@4x.webp';
+export { default as RollingBadge } from './badge/rollring-content-png@4x.webp';
+export { default as TreasureBadge } from './badge/treasure-content-update@4x.webp';
 export { default as MainBanner } from './banner/main-screen-banner.webp';
 export { default as kakaoProfile } from './profile/kakao_profile.jpeg';
