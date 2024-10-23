308727800d24582fafd9a4cea9273edf87ee4d30
bisari31 <bisari31@gmail.com>
2024-10-24 02:00:51 +0900

    feat: 로그인 로딩스피너 추가

--- src/routes/kakako-redirection.tsx
+++ src/routes/kakako-redirection.tsx
@@ -1,13 +1,15 @@
+import styled from '@emotion/styled';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { useTokenQuery } from '@/queries/auth';
 import routes from '@/routes';
+import { StyledBrowserBackdrop } from '@/routes/layout.style';
 import { token } from '@/utils/storage';
 
 export default function KakaoRedirection() {
   const code = new URL(location.href).searchParams.get('code');
-  const { data, isLoading, isError } = useTokenQuery(code);
+  const { data, isError } = useTokenQuery(code);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -24,7 +26,54 @@ export default function KakaoRedirection() {
     }
   }, [code, data, navigate]);
 
-  if (isLoading) return <div>sdfffffffffffff</div>;
   if (isError) throw Error();
-  return null;
+
+  return (
+    <StyledWrapper>
+      <StyledSpinner />
+      <StyledBackdrop />
+      <StyledBrowserBackdrop />
+    </StyledWrapper>
+  );
 }
+const StyledWrapper = styled.div`
+  background-color: ${(props) => props.theme.colors.white};
+  max-width: ${(props) => props.theme.sizes.mobile};
+  margin: 0 auto;
+  width: 100%;
+  padding: ${(props) => `0 ${props.theme.sizes.padding}`};
+  flex-direction: column;
+  display: flex;
+  min-height: 100vh;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledSpinner = styled.div`
+  /* margin: calc(50% - 25px) auto; */
+  width: 100px;
+  height: 100px;
+  z-index: 1;
+  position: relative;
+  border: 10px solid #cbd9ef;
+  box-sizing: border-box;
+  border-left-color: #4788eb;
+  border-radius: 100%;
+  animation: spin 1.2s ease-in-out infinite;
+
+  @keyframes spin {
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
+
+const StyledBackdrop = styled.div`
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  z-index: 0;
+  right: 0;
+  top: 0;
+  background-color: #00000010;
+`;
