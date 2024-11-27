1d71a02181dd8ae114673acc003b54660b71ef47
bisari31 <bisari31@gmail.com>
2024-11-27 20:54:57 +0900

    design: 마이페이지 탭 모션 추가

--- src/components/my/menu/activity-tab.tsx
+++ src/components/my/menu/activity-tab.tsx
@@ -1,8 +1,9 @@
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+import { motion } from 'framer-motion';
+
 import { Document, Edit } from '@/assets/svg/icons';
-import {
-  StyledMenu,
-  StyledMenuWrapper,
-} from '@/components/my/menu/activity-tab.style';
+import { getFontSizeAndWeight } from '@/styles/mixins';
 
 const tab = [
   {
@@ -39,8 +40,57 @@ export default function MyMenuActivityTab({
         >
           <tab.svg />
           <span>{tab.name}</span>
+          {activeIndex === i && <StyledMotion layoutId="underline" />}
         </StyledMenu>
       ))}
     </StyledMenuWrapper>
   );
 }
+
+const StyledMotion = styled(motion.div)`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 1px;
+  background: ${({ theme }) => theme.colors['gray-800']};
+`;
+
+const StyledMenuWrapper = styled.div`
+  display: flex;
+  gap: 8px;
+  position: relative;
+`;
+const StyledMenu = styled.button<{ isActive: boolean }>`
+  ${({ isActive, theme }) => css`
+    ${getFontSizeAndWeight('heading4', isActive ? 'bold' : 'medium')}
+    display: flex;
+    padding: 16px 0;
+    gap: 4px;
+    position: relative;
+    justify-content: center;
+    align-items: center;
+    flex: 1;
+    /* &::after {
+      display: ${isActive ? 'auto' : 'none'};
+      content: '';
+      position: absolute;
+      width: 100%;
+      height: 1px;
+      bottom: 0;
+      border-bottom: 1px solid ${theme.colors['gray-800']};
+    } */
+    svg {
+      path {
+        stroke: ${isActive
+          ? theme.colors['gray-800']
+          : theme.colors['gray-500']};
+      }
+      width: 20px;
+      height: 20px;
+    }
+    span {
+      color: ${isActive ? theme.colors['gray-900'] : theme.colors['gray-600']};
+    }
+  `}
+`;
--- src/components/my/menu/category-tab.tsx
+++ src/components/my/menu/category-tab.tsx
@@ -1,5 +1,5 @@
-import { css } from '@emotion/react';
 import styled from '@emotion/styled';
+import { motion } from 'framer-motion';
 
 import { getFontSizeAndWeight } from '@/styles/mixins';
 
@@ -26,6 +26,7 @@ export default function MyMenuCategoryTab({
           isActive={activeTab === content.id}
           key={content.id}
         >
+          {activeTab === content.id && <StyledMotion layoutId="bubble" />}
           {content.label}
         </StyledButton>
       ))}
@@ -40,15 +41,25 @@ const StyledWrapper = styled.div`
   background-color: ${({ theme }) => theme.colors['gray-100']};
 `;
 
+const StyledMotion = styled(motion.span)`
+  position: absolute;
+  width: 100%;
+  z-index: 10;
+  inset: 0;
+  border-radius: 4px;
+  mix-blend-mode: color;
+  background-color: ${({ theme }) => theme.colors['powderBlue-900']};
+  color: ${({ theme }) => theme.colors.white};
+`;
+
 const StyledButton = styled.button<{ isActive: boolean }>`
   border-radius: 4px;
+  -webkit-tap-highlight-color: transparent;
+  position: relative;
+  z-index: 11;
   ${getFontSizeAndWeight('heading4', 'regular')}
   padding: 12px 16px;
   flex: 1;
-  ${({ theme, isActive }) => css`
-    background-color: ${isActive
-      ? theme.colors['powderBlue-900']
-      : theme.colors['gray-100']};
-    color: ${isActive ? theme.colors.white : theme.colors['gray-500']};
-  `}
+  color: ${({ theme, isActive }) =>
+    isActive ? theme.colors.white : theme.colors['gray-500']};
 `;
